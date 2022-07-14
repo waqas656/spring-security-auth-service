0e5e52ad5789260166b4cf20ed23ab3741daa6be
Waqas Ahmed <waqasmlik565@gmail.com>
2022-07-14 18:12:23 +0500

    First Commit
    Basic project with dependencies for spring security, web, jpa and tests

---
+++ src/main/java/com/springsecurityauth/springsecurityauthservice/SpringSecurityAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurityauth.springsecurityauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthServiceApplication.class, args);
+	}
+
+}
